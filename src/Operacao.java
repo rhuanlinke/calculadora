0d222a6c30d01c97afb37943f1b6691a355ec3f9
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-10-25 20:15:31 -0300

    Comentário indevido

--- src/Operacao.java
+++ src/Operacao.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Operacao {
 	
-	//faz o calculo se for válido.. foi decisão minha fazer só um método para todas as 4 operações
 	public void Operacao (int operacao, double nro1, double nro2) {
 		if (operacao == 1) {
 			Soma soma = new Soma();
